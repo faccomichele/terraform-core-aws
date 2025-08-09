f4a2aef274a65beab6b7c7e7153788501532ef5e
Michele Facco <faccomichele@gmail.com>
2025-08-09 10:55:17 +0200

    adding param export for automation

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,10 @@
+output "backend_configuration_hcl_parameter_name" {
+  description = "The name of the SSM parameter storing the backend configuration HCL."
+  value       = aws_ssm_parameter.backend_configuration_hcl.name
+}
+
 output "backend_configuration_hcl" {
   description = "Backend configuration in HCL format for copy-paste into other Terraform configurations"
-  value = <<-EOT
-terraform {
-  backend "s3" {
-    bucket        = "${aws_s3_bucket.terraform_state.id}"
-    key           = "CHANGE_ME/terraform.tfstate"
-    region        = "${data.aws_region.current.name}"
-    role_arn      = "${aws_iam_role.terraform_state_role.arn}"
-    encrypt       = true
-    use_lockfile  = true
-  }
-}
-EOT
+  value       = aws_ssm_parameter.backend_configuration_hcl.value
+  sensitive   = true
 }
---
+++ ssm.tf
@@ -0,0 +1,18 @@
+resource "aws_ssm_parameter" "backend_configuration_hcl" {
+	name        = "/terraform-core/backend_configuration_hcl"
+	description = "Backend configuration in HCL format for copy-paste into other Terraform configurations"
+	type        = "SecureString"
+	value       = <<-EOT
+terraform {
+	backend "s3" {
+		bucket        = "${aws_s3_bucket.terraform_state.id}"
+		key           = "CHANGE_ME/terraform.tfstate"
+		region        = "${data.aws_region.current.name}"
+		role_arn      = "${aws_iam_role.terraform_state_role.arn}"
+		encrypt       = true
+		use_lockfile  = true
+	}
+}
+EOT
+	overwrite   = true
+}
